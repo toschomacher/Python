65dba7c746475676ef10fc227e57f8e660629848
Todor Vasilev <114831156+toschomacher@users.noreply.github.com>
2022-10-13 00:02:57 +0100

    Update WeekThree.py

--- WeekThree.py
+++ WeekThree.py
@@ -65,21 +65,72 @@ if __name__ == '__main__':
                 print(f"Password can not contain the word {password}")
         else:
             print("Error: Password must be between 8 and 12 characters.")
+
     # Task 6
     print('\n*********** Task 6 ***********')
     print('\tSeven Time Table:')
-    print(' _______________________________________')
-    print('|Opera- | Multi-|  Multi- | Result      |')
-    print('|tion # |plicand|  plier  | Product     |')
-    print('|_______|_______|_________|_____________|')
+    print(' __________________________________')
+    print('|Opera- | Multi-| Multi- | Result/|')
+    print('|tion # |plicand| plier  | Product|')
+    print('|_______|_______|________|________|')
     
-    number = 0
+    count = 1
     multiplicand = 7
     
-    for mult in range(12):
-        number += 1
-        mult += 1
-        print('|', number, f'\t|  {multiplicand}', '   x', f'  {number}', '\t  | ', mult*multiplicand, ' \t|')
-    print('|_______|_______|_________|_____________|')
-    print()
+    for mult in range(13):
+        print(f'|{count:2}', f'\t|  {multiplicand}', '   x', f'  {mult}', '\t =  ', f'{mult*multiplicand:2}', '  |')
+        count +=1
+    
+    print('|_______|_______|________|________|')
+    
+    # Task 7
+    print('\n*********** Task 7 ***********')
+    print('\tChoose A Time Table:')
+    
+    count = 1
+    multiplicand = int(input('Choose a multiplicand from 0 to 12: '))
+    
+    print(' ___________________________________')
+    print('|Opera- | Multi-| Multi- | Result/ |')
+    print('|tion # |plicand| plier  | Product |')
+    print('|_______|_______|________|_________|')
+    
+    for mult in range(13):
+        print(f'|{count:2}', f'\t|  {multiplicand:2}', '  x', f'  {mult:2}', '  =  ', f'{mult*multiplicand:3}', '  |')
+        count +=1
+        
+    print('|_______|_______|________|_________|')
+    
+    # Task 8
+    print('\n*********** Task 8 ***********')
+    print('\tChoose A Time Table:')
+    
+    while True:
+        multiplicand = int(input('Choose a multiplicand from -12 to +12: '))
+        if 0 <= multiplicand < 13:
+            break
+        elif -13 < multiplicand < 0:
+            break
+        else:
+            print('Error: Invalid number.\nEnter a number between -12 and 12 inclusive.')
+    print(' ___________________________________')
+    print('|Opera- | Multi-| Multi- | Result/ |')
+    print('|tion # |plicand| plier  | Product |')
+    print('|_______|_______|________|_________|')
+    
+    start_range = 0
+    finish_range = 13
+    count = 1
+    
+    if multiplicand <0:
+        multiplicand *=-1
+        start_range = -12
+        finish_range = 1
+    
+    for mult in range(start_range,finish_range):
+        mult = mult * -1
+        print(f'|{count:2}', f'\t|  {multiplicand:2}', '  x', f'  {mult:2}', '  =  ', f'{mult*multiplicand:3}', '  |')
+        count +=1
+    
+    print('|_______|_______|________|_________|')
     
