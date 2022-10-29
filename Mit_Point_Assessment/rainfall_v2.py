5771c9380010723362d5c09526adc4abd12b84bd
Todor Vasilev <114831156+toschomacher@users.noreply.github.com>
2022-10-29 01:35:32 +0100

    Create rainfall_v2.py

---
+++ Mit_Point_Assessment/rainfall_v2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import calendar
+import statistics
+from statistics import mean as avg
+if __name__ == '__main__':
+    rainfall = ['max','min']
+    
+    for month in (calendar.month_name[1:]):
+        user_input = (input(f"Enter rainfall for {month}: "))
+        user_input = int(user_input[:-2])
+        if all(i <= user_input for i in rainfall[2:]):
+            rainfall[0] = month
+        if all(i >= user_input for i in rainfall[2:]):
+            rainfall[1] = month
+        rainfall.append(user_input)
+        
+    print(f"\nMax Rainfall: {max(rainfall[2:])}mm in {rainfall[0]}")
+    print(f"Min Rainfall: {min(rainfall[2:])}mm in {rainfall[1]}\n")
+    print(f"Average: {avg(rainfall[2:]):.2f}mm.")
+    print(f"Standard Deviation: {statistics.stdev(rainfall[2:]):.2f}mm.")
