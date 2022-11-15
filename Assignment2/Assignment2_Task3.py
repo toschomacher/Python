e6c6f09d349bc60d8e0f8daefb9c678bc496b0fe
Todor Vasilev <114831156+toschomacher@users.noreply.github.com>
2022-11-15 12:29:05 +0000

    Update Assignment2_Task3.py

--- Assignment2/Assignment2_Task3.py
+++ Assignment2/Assignment2_Task3.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     my_list = process.list_process(data_into_list)  # print(my_list[0][:]) print(my_list[1][:]) print(my_list[2][:])
     final_list = []
     for n in range(0, len(my_list[0][:])):
-        final_list.append(my_list[0][n] + " " + my_list[1][n] + "@poppleton.ac.uk")
+        final_list.append(my_list[0][n] + " " + my_list[2][n] + my_list[1][n] + "@poppleton.ac.uk")
     print(final_list)
