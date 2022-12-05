6accfb7b728c12b1ac683876310781cb7098b214
Todor Vasilev <114831156+toschomacher@users.noreply.github.com>
2022-12-05 20:59:41 +0000

    Create constants.py

---
+++ Assignment2/constants.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+WORDS = ["one", "two", "three", "four", "five", "six", "seven", "eight",
+                 "Alex", "Benji", "Caren", "David", "Emily", "Frank", "George", "Hudson",
+                 "Zunisha", "Yellow", "Xylophone", "White", "Violet", "Unicorn", "Turquoise", "Silver"]
