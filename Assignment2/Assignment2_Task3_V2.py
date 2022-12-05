651f081f86b54905ac71571043ee0816958db695
Todor Vasilev <114831156+toschomacher@users.noreply.github.com>
2022-12-05 21:02:06 +0000

    Update Assignment2_Task3_V2.py

--- Assignment2/Assignment2_Task3_V2.py
+++ Assignment2/Assignment2_Task3_V2.py
@@ -1,59 +1,53 @@
 #!/usr/bin/env python3
 
-import sys
-
-
 def list_process(expect_list):
-
-    from re import sub
+    from re import sub as replace
     names_initials = []
     temp_string = ""
-
     ucas_list = [k[:8] for k in expect_list]
     temp_list = [k[9:] for k in expect_list]
     surnames_list = [k[:k.index(",")] for k in temp_list]
     first_names_list = [k[k.index(",")+1:] for k in temp_list]
     surnames_list = [element.lower() for element in surnames_list]
-
     for x in surnames_list:
-        new_string = sub(r'[^a-zA-Z]', '', x)
+        new_string = replace(r"[^a-zA-Z]", "", x)
         surnames_list[surnames_list.index(x)] = new_string
-
     for element in first_names_list:
         for c in range(0, len(element)):
-            if element[c] == ' ':
-                temp_string += element[c+1].lower() + '.'
+            if element[c] == " ":
+                temp_string += element[c+1].lower() + "."
             if c == len(element)-1:
                 names_initials.append(temp_string)
                 temp_string = ""
-
     return ucas_list, surnames_list, names_initials
 
 
-def number_generator():
+def four_digits():
+    from string import digits
+    from random import choice
+    return ''.join([choice(digits) for _ in range(4)])
 
-    from random import randint
-    rand_number = ""
-    for num in range(4):
-        rand_number += str(randint(0, 9))
 
-    return rand_number
+def final_format(data_tuple):
+    return [(data_tuple[0][n] + " " + data_tuple[2][n] + data_tuple[1][n] + four_digits() +
+             "@poppleton.ac.uk") for n in range(len(data_tuple[0]))]
+
+
+def write_file(print_list):
+    with open("students_emails.txt", "w") as f:
+        for b in range(len(print_list)):
+            f.write(str(print_list[b]) + "\n")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
+    from sys import argv as file
+    file_name = file[1]
+    if len(file) == 1:
         print("Error: Missing command-line argument.")
     else:
         try:
-            my_file = open(f"{sys.argv[1]}", "r")
-            my_tuple = list_process(my_file.readlines())
-            print_list = [(my_tuple[0][n] + " " + my_tuple[2][n] + my_tuple[1][n] + number_generator() +
-                           "@poppleton.ac.uk") for n in range(len(my_tuple[0]))]
-
-            with open('students_emails.txt', 'w') as f:
-                for b in range(len(print_list)):
-                    f.write(str(print_list[b]) + '\n')
-
-            my_file.close()
+            read_file = open(f"{file_name}", "r")
+            write_file(final_format(list_process(read_file.readlines())))
+            read_file.close()
         except FileNotFoundError:
-            print('Error: Cannot open "' + sys.argv[1] + '". Sorry about that.')
+            print('Error: Cannot open "' + file_name + '". Sorry about that.')
