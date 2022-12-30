8ae108f8d0442ffe12c806e36e38abaa71b38b3e
Todor Vasilev <114831156+toschomacher@users.noreply.github.com>
2022-12-30 15:46:03 +0000

    Update Circles.py

--- Circles.py
+++ Circles.py
@@ -5,12 +5,23 @@ if __name__ == "__main__":
     counter = 1
     num_of_circles = int(input("Enter number of circles: "))
     result = []
+    r1 = 0.0
+    r2 = 0.0
     for n in range(num_of_circles):
-        print("Enter r" + str(counter), " value: ", end="")
-        counter += 1
-        r1 = float(input(""))
-        result.append(r1)
-        print("Enter r" + str(counter), " value: ", end="")
+        if r1 == 0.0:
+            print("Enter starting value for r" + str(counter), " value: ", end="")
+            counter += 1
+            r1 = float(input(""))
+            result.append(r1)
+        else:
+            r1 = round((r2 + 0.2), 1)
+            result.append(r1)
+            print("Value for r" + str(counter), " = ", r1, "\n", end="")
+            counter += 1
+        if n == num_of_circles-1:
+            print("Enter r" + str(counter), "value (final): ", end="")
+        else:
+            print("Enter r" + str(counter), "value: ", end="")
         counter += 1
         r2 = float(input(""))
         result.append(r2)
@@ -21,5 +32,5 @@ if __name__ == "__main__":
         area = (r2 - r1) / 2 * circumference
         result.append(f"{area:.2f}")
     for x in range(num_of_circles):
-        print("Circle", x+1, " with R1=", result[x*5], " R2=", result[x*5+1], " avR=", result[x*5+2],
-              " C=", result[x*5+3], " A=", result[x*5+4])
+        print("Circle ", x+1, " with Rin=", f"{result[x*5]:5.2f}", " Rout=", f"{result[x*5+1]:5.2f}",
+              " Ravrg=", result[x*5+2], " Circ=", result[x*5+3], " Area=", result[x*5+4], "\n", end="")
